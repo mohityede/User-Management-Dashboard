6b220ed4e9a4aadf1334cb41caf3de76036d826d
mohityede <mohityede755@gmail.com>
2023-11-17 11:25:59 +0530

    login component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-red-500'>app</h1>
+    <div className="App min-h-screen flex items-center bg-gradient-to-br from-purple-300 to-green-400">
+      <div className="max-w-3xl mx-auto">
+        <div>
+          <div className="relative w-max mx-auto h-12 grid grid-cols-2 items-center px-[3px] rounded-full bg-gray-900/20 overflow-hidden shadow-2xl shadow-900/20 transition">
+            <button className="relative block h-10 px-6 tab rounded-full">
+              <span className="text-gray-800">Users Details</span>
+            </button>
+            <button className="bg-green-50 relative block h-10 px-6 tab rounded-full">
+              <span className="text-gray-800">Account Creation</span>
+            </button>
+          </div>
+          <div className="mt-6 rounded-2xl bg-green-50">
+            {/* <Dashboard /> */ }
+            <Login />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+
+function Dashboard() {
+    return (
+        <div className="p-3">
+            <h1>Dashboard</h1>
+            <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo aperiam cum, reiciendis, asperiores vitae accusamus porro laudantium dolores consequuntur aspernatur eius odio maxime eveniet, repellendus nesciunt reprehenderit soluta praesentium dolorum.</h2>
+            <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo aperiam cum, reiciendis, asperiores vitae accusamus porro laudantium dolores consequuntur aspernatur eius odio maxime eveniet, repellendus nesciunt reprehenderit soluta praesentium dolorum.</h2>
+        </div>
+    );
+}
+
+export default Dashboard;
